cb57115855a74dc26761aa42254fcc160987355b
Heitor Belem <heitorlbelem98@gmail.com>
2024-08-22 21:49:40 -0300

    Creates configuration for webpack, json-server and babel

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+import "./styles/global.css"
+
+import "./styles/utilities/main.css"
+import "./styles/utilities/text.css"
+import "./styles/utilities/display.css"
+
+import "./styles/components/header.css"
+import "./styles/components/hero.css"
+import "./styles/components/loyalty-card.css"
+import "./styles/components/progress.css"
+import "./styles/components/history.css"
